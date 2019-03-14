2ad88f4dd4cfa625a2959e67e104e927c32e5b56
lsqaisen <lai413633@outlook.com>
2019-03-14 15:15:47 +0800

    use typescript

--- .umirc.js
+++ .umirc.js
@@ -12,7 +12,8 @@ export default {
         webpackChunkName: true
       },
     }],
-    ['../utils/plugins/plugin.js', {
+    ['mife', {
+      type: 'plugin',
       dynamicImport: true,
       publicPath: '/lib/',
       externals: {
---
+++ typings.d.ts
@@ -0,0 +1,13 @@
+declare module '*.less';
+declare module 'dva';
+declare module 'umi';
+declare module 'umi/withRouter';
+declare module 'umi/link';
+declare module 'umi/router';
+declare module 'antd';
+declare module 'plugin-global';
+
+interface Window {
+  mife_menus?: object;
+}
+declare var window: Window;
