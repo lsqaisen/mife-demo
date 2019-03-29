76a29fd4ba2df3e063b51ee64dc5d25fe217b029
lsqaisen <lai413633@outlook.com>
2019-03-29 15:40:13 +0800

    modify

--- .umirc.js
+++ .umirc.js
@@ -3,13 +3,18 @@ export default {
   plugins: [
     ['umi-plugin-react', {
       dva: true,
+      antd: {
+        "libraryName": "antd",
+        "libraryDirectory": "es",
+        "style": true // `style: true` 会加载 less 文件
+      },
       routes: {
         exclude: [
           /model/,
         ],
       },
       dynamicImport: {
-        webpackChunkName: true
+        webpackChunkName: false,
       },
     }],
     ['mife', {
@@ -21,7 +26,22 @@ export default {
         'react-dom': 'window.ReactDOM',
         'dva': 'window.dva',
       },
-    }]
+    }],
   ],
   hash: true,
+  copy: [{ from: './src/public/oem', to: './static/oem', toType: 'dir' },],
+  chainWebpack(config, { webpack }) {
+    config.resolve.extensions.add(".tsx");
+  },
+  alias: {
+    '@': './src/components/'
+  },
+  define: {
+    "process.env.OEM_NAME": '/kubeup'
+  },
+  theme: {
+    "@primary-color": "#2D225A"
+  },
+  proxy: {
+  },
 }
--- typings.d.ts
+++ typings.d.ts
@@ -1,13 +1,6 @@
 declare module '*.less';
-declare module 'dva';
-declare module 'umi';
-declare module 'umi/withRouter';
-declare module 'umi/link';
-declare module 'umi/router';
-declare module 'antd';
-declare module 'plugin-global';
+declare module 'react-lifecycles-compat';
 
 interface Window {
   mife_menus?: object;
 }
-declare var window: Window;
